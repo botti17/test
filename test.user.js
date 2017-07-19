c75269eebedeffdd14e499eb86432547e8df7c8b
botti17 <s390610@yahoo.co.jp>
2017-07-19 19:58:47 +0900

    初版

---
+++ test.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name        test
+// @namespace   http://*.3gokushi.jp/village.php*
+// @description 六角次導化 by ぼっち@yabage17w
+// @include     http://*.3gokushi.jp/village.php*
+// @include     http://*.3gokushi.jp/card/deck.php*
+// @include     http://*.3gokushi.jp/user*
+// @exclude	http://*.3gokushi.jp/maintenance*
+// @require	http://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
+// @require     http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js
+// @connect	3gokushi.jp
+// @grant GM_getValue
+// @grant GM_setValue
+// @grant GM_xmlhttpRequest
+// @grant GM_log
+// @version 0.13
+// ==/UserScript==
+
+//2017.04.16 ツールの作成に着手
+//
